541c06b71c238317634100e611acee67dd23bfc2
Chase Mack <chase.mack@yahoo.com>
2022-03-05 00:00:47 -0500

    Created and added Footer styled component

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,7 +3,8 @@ import { Navbar, NavList, ListItem, NavLink } from './Styles/NavbarStyles';
 import { Intro } from './Styles/IntroStyles';
 import { Container } from './Styles/ContainerStyles';
 import { MissionStatement } from './Styles/MissionStatementStyles';
-import { Projects, Card } from './Styles/ProjectStyles';
+import { Projects, Card } from './Styles/ProjectsStyles';
+import { Footer } from './Styles/FooterStyles';
 
 class App extends React.Component {
     render() {
@@ -38,7 +39,7 @@ class App extends React.Component {
                 <main>
                     <Intro>
                         <Container>
-                            <h1>Hi, my name is <span style={{color: 'rgb(255, 40, 75)'}}>Chase Mack</span>
+                            <h1>Hi, my name is <span style={{ color: 'rgb(255, 40, 75)' }}>Chase Mack</span>
                                 <br />
                                 And I'm a Web Developer.
                             </h1>
@@ -46,9 +47,9 @@ class App extends React.Component {
                     </Intro>
                     <MissionStatement>
                         <Container smmedia>
-                            <h2 style={{fontSize: '2rem'}}>A Little About Me</h2>
-                            <hr style={{width: '20rem'}}/>
-                            <p style={{fontSize: '1.5rem'}}>
+                            <h2 style={{ fontSize: '2rem' }}>A Little About Me</h2>
+                            <hr style={{ width: '20rem' }} />
+                            <p style={{ fontSize: '1.5rem' }}>
                                 With my background in the energy industry, I have learned value lies in delivering
                                 the highest quality product in the shortest amount of time possible. Those principles
                                 will translate but I knew I wanted that product to change while working on my first
@@ -62,9 +63,13 @@ class App extends React.Component {
 
                     </Projects>
                 </main>
-                <footer>
-
-                </footer>
+                <Footer>
+                    <NavList>
+                        <ListItem>
+                            <NavLink></NavLink>
+                        </ListItem>
+                    </NavList>
+                </Footer>
             </React.Fragment>
         )
     }
