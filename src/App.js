9755338c32fa95f4f22f177dfbbcba1df884d192
Chase Mack <chase.mack@yahoo.com>
2022-03-29 22:12:52 -0400

    Removed fontawesome library from app.js

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import GlobalStyle from './GlobalStyles';
 import { Header } from './Components/Header';
 import { Intro } from './Components/Intro';
 import { Footer } from './Components/Footer';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { AboutMe } from './Components/AboutMe';
 import { Projects } from './Components/Projects';
-
-library.add(fab, faEnvelope);
 
 class App extends React.Component {
     render() {
