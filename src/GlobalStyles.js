45110a8a38c460ca25efb59b618eef87dc10c43c
Chase Mack <chase.mack@yahoo.com>
2022-04-18 23:01:29 -0400

    Trying to remove whitespace

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -4,7 +4,7 @@ const GlobalStyle = createGlobalStyle`
     html, body {
         width: 100%;
         height: 100%;
-        overflow-x: clip;
+        overflow-x: hidden;
         margin: 0;
         padding: 0;
         scroll-behavior: smooth;
